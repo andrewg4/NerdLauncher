13217e26b439de6e4cea85e97036ed27cee1326b
Mygitcode <andrey.gulak94@gmail.com>
2015-06-25 16:29:50 +0300

    [BG-1] Create NerdLauncherFragment class and layout

---
+++ app/src/main/java/ua/com/prologistic/nerdlauncherproject/NerdLauncherActivity.java
@@ -0,0 +1,11 @@
+package ua.com.prologistic.nerdlauncherproject;
+
+import android.support.v4.app.Fragment;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new NerdLauncherFragment();
+    }
+}
